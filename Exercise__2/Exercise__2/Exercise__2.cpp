5a8611b7d3bd02c56089520cd771a11a8570195d
Naqib Hakimi <55173775+naqibhakimi24@users.noreply.github.com>
2019-09-24 12:45:31 +0800

    Add files via upload

---
+++ Exercise__2/Exercise__2/Exercise__2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "*************************************\n";
+	cout << "*****\t\tUTEM\t\t*****\n";
+	cout << "*************************************\n\n";
+	cout << "10\n";
+	cout << "9\n";
+	cout << "8\n";
+	cout << "7\t    X\n";
+	cout << "6 X\n";
+	cout << "5\t\t    X\n";
+	cout << "4   X\n";
+	cout << "3\t\tX\n";
+	cout << "2     X\n";
+	cout << "1 X\n";
+	cout << "0 1 2 3 4 5 6 7 8 9 10\n";
+
+	return 0;
+}
+
