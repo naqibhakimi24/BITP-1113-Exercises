26f700516f8a955409a9823222b2ce794622023e
Naqib Hakimi <55173775+naqibhakimi24@users.noreply.github.com>
2019-09-24 12:44:01 +0800

    Add files via upload

---
+++ Bio1/Bio1/Bio1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "University:\tUniversiti Teknikal Malaysia, Melaka\n\n";
+	cout << "Faculty:\tFTMK\n\n";
+	cout << "Programme:\tBITI\n\n";
+	cout << "My Name is Muhammad Naqib Hakimi Bin Hakmal. I am from Selangor\n";
+	return 0;
+
+}
